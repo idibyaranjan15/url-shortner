c91fb542ff3c1a1ead91026012d2c68d92b7ce30
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-11-27 07:03:07 +0000

    Url-shortner

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import connectDb from "./db/db.js";
-
+import { PORT } from "./constants/constants.js";
+import mainRouter from "./routes/url.routes.js";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use("/api/v1", mainRouter);
 connectDb();
-app.listen("/", () => {
+
+app.listen(PORT, () => {
   console.log(`Server is listening in  DopeMode on ${PORT}`);
 });
