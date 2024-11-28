63dc5ed9f0d9b61b20ffe205c4d946ee3fccf6a1
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2024-11-28 12:23:21 +0530

    adding view and static routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDb from "./db/db.js";
+import path from "path";
 import { PORT } from "./constants/constants.js";
 import mainRouter from "./routes/url.routes.js";
 import cors from "cors";
@@ -7,8 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1", mainRouter);
-connectDb();
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.listen(PORT, () => {
+  connectDb();
   console.log(`Server is listening in  DopeMode on ${PORT}`);
 });
--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -4,8 +4,23 @@ import {
   handleGetAnalytics,
   hanlderedirectURL,
 } from "../controllers/url.controllers.js";
+import Url from "../model/url.model.js";
 
 const router = Router();
+
+router.get("/test", async (req, res) => {
+  try {
+    const allUrls = await Url.find({});
+
+    return res.render("home", {
+      urls: allUrls,
+      host: req.headers.host,
+    });
+  } catch (error) {
+    console.error("Error fetching URLs:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 router.post("/url", handleGenerateNewShortURL);
 
